158852d23a2d448de7a43314e84b78b28d56b0c4
aubbhj <aubbhj@gmail.com>
2022-10-10 18:57:35 +0100

    kkk

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: abelhadj <abelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 17:35:24 by abelhadj          #+#    #+#             */
-/*   Updated: 2022/10/10 18:16:46 by abelhadj         ###   ########.fr       */
+/*   Updated: 2022/10/10 18:57:19 by abelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (0);
 	ft_memcpy(str, s1, ft_strlen(s1));
 	ft_memcpy(str + ft_strlen(s1), s2, ft_strlen(s2));
-	str[len] = '\0';
+	
 	return (str);
 }
