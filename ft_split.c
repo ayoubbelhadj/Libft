34d317d05fa18d4b3b41e6f21db993795de2e505
aubbhj <aubbhj@gmail.com>
2022-10-14 15:44:12 +0100

    14-10-22 1.1

---
+++ ft_split.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelhadj <abelhadj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 15:43:30 by abelhadj          #+#    #+#             */
+/*   Updated: 2022/10/14 15:43:32 by abelhadj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_count(char *s, char c)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+		{
+			i++;
+			j++;
+		}
+		if (j > 0)
+		{
+			k++;
+			j = 0;
+		}
+	}
+	return (k);
+}
+
+int	ft_count2(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	**ft_fill(char **t, char *s, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+		{
+			t[j] = malloc(sizeof(char) * (ft_count2((char *)s + i, c) + 1));
+			if (!t[j])
+				return (0);
+			while (s[i] && s[i] != c)
+				t[j][k++] = s[i++];
+			t[j++][k] = 0;
+			k = 0;
+		}
+	}
+	t[j] = 0;
+	return (t);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**t;
+
+	t = (char **)malloc(sizeof(char *) * (ft_count((char *)s, c) + 1));
+	if (!t)
+		return (0);
+	return (ft_fill(t, (char *)s, c));
+}
